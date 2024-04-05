ecfc56e876538d39f6853584ff7c815ab3320663
Manoharan777 <mano1000.vm@gmail.com>
2024-04-05 17:59:16 +0530

    Fix: URL changed to https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,17 @@
-
 const GOOGLE_API_KEY = "AIzaSyDCkn8lcT8WSo0dnrFYOUq1UO-YMW0KgvU";
 
-export const YOUTUBE_VIDEOS_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+export const YOUTUBE_VIDEOS_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-  export const SEARCH_QUERY_DATA =
-    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&regionCode=IN&key=" +
-    GOOGLE_API_KEY + "&q=";
+export const SEARCH_QUERY_DATA =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY +
+  "&q=";
 
-    export const LIVE_CHAT_COUNT = 25;
+export const LIVE_CHAT_COUNT = 25;
 
-  //&q=namastejavascript
+//&q=namastejavascript
